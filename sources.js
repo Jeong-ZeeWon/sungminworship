f162701ea51eb73da6a31a98432e79c83ae0b332
ZeeWon <baysuss@gmail.com>
2026-05-14 19:39:44 +0900

    docs: add notion source metadata

---
+++ sources.js
@@ -0,0 +1,33 @@
+const sourceMeta = {
+  sourceType: 'notion-static-snapshot',
+  description: '이 앱의 예배 일정과 큐티 데이터는 목회자가 관리하는 Notion 원자료를 바탕으로 data.js에 정적 스냅샷으로 옮겨 담은 것입니다. 현재 앱이 브라우저에서 Notion API를 실시간 호출하지는 않습니다.',
+  lastSyncedAt: '2026-05-14',
+  sections: {
+    sunday: {
+      label: '주일예배',
+      notionTitle: '주일예배',
+      notionUrl: 'https://www.notion.so/2df5e604d8c6816481c6f31a7070a2d4?pvs=1'
+    },
+    wednesday: {
+      label: '수요예배',
+      notionTitle: '수요예배 담당자',
+      notionUrl: 'https://www.notion.so/2e15e604d8c6802f8284dd7ccc553b4f?pvs=1'
+    },
+    friday: {
+      label: '금요성령집회',
+      notionTitle: '금요성령집회',
+      notionUrl: 'https://www.notion.so/2e35e604d8c681df8d57cdbab62ed6ec?pvs=1',
+      note: '2026-05-15 특별강사 일정은 사용자 제공 수정사항을 반영했습니다.'
+    },
+    qt: {
+      label: '큐티 본문',
+      notionTitle: '매일의 향기',
+      notionUrl: 'https://www.notion.so/2df5e604d8c68153b64dc76accb69102?pvs=1'
+    },
+    leaders: {
+      label: '한 주간 예배 담당자',
+      notionTitle: '한 주간 예배 담당자',
+      notionUrl: 'https://www.notion.so/35b5e604d8c680aea0c4d47fc08c83f7?pvs=1'
+    }
+  }
+};
